12a9147eb8328da39504c4a6007e5b1edac897ba
GyuJin Choi <mcqueen.choi@gmail.com>
2025-10-10 17:58:56 +0900

    Feat/63/settings scene (#67)
    * [Feat] SettingSceneController
    
    * [Feat] save settings
    
    * [Feat] Custom Settings and Popup Page
    
    * [Fix] property 저장 시도 시 settingservice null pointer 나는 문제 해결
    
    TODO : property 불러올 때 값 날아가는 현상 해결
    
    * [Fix] Custom Setting 선택 시 값 날아가는 현상 수정
    
    TODO : 버튼 선택 해도 적용 안되는 문제 수정
    
    * [Fix] Custom Setting 적용 가능하도록 수정

--- tetris-client/src/main/java/seoultech/se/client/controller/CustomSettingPopController.java
+++ tetris-client/src/main/java/seoultech/se/client/controller/CustomSettingPopController.java
@@ -1,33 +1,82 @@
 package seoultech.se.client.controller;
 
-import org.springframework.stereotype.Component;
+import java.util.HashMap;
+import java.util.Map;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import seoultech.se.client.model.Setting;
+
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import seoultech.se.client.config.ApplicationContextProvider;
+import seoultech.se.client.service.SettingsService;
+
 
 @Component
 public class CustomSettingPopController extends BaseController {
+    private CustomSettingSceneController mainController;
 
     @FXML
-    private TextField settingNameField; //사용자가 이름 입력하는 field
+    private TextField settingNameField;
     @FXML
     private Button saveButton;
     @FXML
     private Button cancelButton;
 
+    @Override
+    @FXML
+    public void initialize() {
+        super.initialize();
+        this.settingsService = ApplicationContextProvider.getApplicationContext().getBean(seoultech.se.client.service.SettingsService.class);
+    }
+
+    public void setMainController(CustomSettingSceneController controller) {
+        this.mainController = controller;
+    }
+
     @FXML
     private void handleSaveButton() {
-        //구현 필요
+        String settingName = settingNameField.getText().trim();
+        if (!settingName.isEmpty()) {
+            Setting newSetting = new Setting(settingName);
+            Map<String, String> configurations = new HashMap<>();
+
+            // TODO : results in null pointer exception
+            try{
+                // Get current settings from SettingsService
+                configurations.put("soundVolume", String.valueOf(settingsService.soundVolumeProperty().getValue()));
+                configurations.put("colorMode", settingsService.colorModeProperty().getValue());
+                configurations.put("screenSize", settingsService.screenSizeProperty().getValue());
+                configurations.put("stageWidth", String.valueOf(settingsService.stageWidthProperty().getValue()));
+                configurations.put("stageHeight", String.valueOf(settingsService.stageHeightProperty().getValue()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+            newSetting.setConfigurations(configurations);
+            newSetting.setSelected(true); // Make this the active setting
+            mainController.addSetting(newSetting);
+            closeWindow();
+        }
     }
 
     @FXML
     private void handleCancelButton() {
-        //구현 필요
+        closeWindow();
     }
 
     @FXML
-    private void handleDeleteButton() {
-        //구현 필요(input 입력 받았을 때 행동)
+    private void handleInputAction() {
+        
+    }
+
+    private void closeWindow() {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
 }
---
+++ tetris-client/src/main/java/seoultech/se/client/repository/SettingsRepository.java
@@ -0,0 +1,90 @@
+package seoultech.se.client.repository;
+
+import org.springframework.stereotype.Repository;
+import seoultech.se.client.model.Setting;
+
+import java.io.*;
+import java.util.*;
+
+@Repository
+public class SettingsRepository {
+    private static final String SETTINGS_FILE = "custom_settings.properties";
+    private Properties properties;
+
+    public SettingsRepository() {
+        properties = new Properties();
+        loadProperties();
+    }
+
+    private void loadProperties() {
+        try (FileInputStream in = new FileInputStream(SETTINGS_FILE)) {
+            properties.load(in);
+        } catch (IOException e) {
+            // File doesn't exist yet - that's okay for first run
+        }
+    }
+
+    private void saveProperties() {
+        try (FileOutputStream out = new FileOutputStream(SETTINGS_FILE)) {
+            properties.store(out, "Tetris Custom Settings");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Setting> loadSettings() {
+        List<Setting> settings = new ArrayList<>();
+        Set<String> settingNames = new HashSet<>();
+
+        // First pass: Create settings with their basic properties
+        for (String key : properties.stringPropertyNames()) {
+            if (key.endsWith(".name")) {
+                String baseName = key.substring(0, key.length() - 5);
+                String name = properties.getProperty(key);
+                boolean selected = Boolean.parseBoolean(properties.getProperty(baseName + ".selected", "false"));
+                
+                Setting setting = new Setting(name);
+                setting.setSelected(selected);
+                
+                // Load configurations for this setting
+                Map<String, String> configs = new HashMap<>();
+                for (String propKey : properties.stringPropertyNames()) {
+                    if (propKey.startsWith(baseName + ".config.")) {
+                        String configKey = propKey.substring((baseName + ".config.").length());
+                        String configValue = properties.getProperty(propKey);
+                        configs.put(configKey, configValue);
+                    }
+                }
+                setting.setConfigurations(configs);
+                
+                settings.add(setting);
+                settingNames.add(name);
+            }
+        }
+
+        return settings;
+    }
+
+    public void saveSettings(List<Setting> settings) {
+        properties.clear();
+        for (Setting setting : settings) {
+            String baseKey = setting.getKey();
+            properties.setProperty(baseKey + ".name", setting.getName());
+            properties.setProperty(baseKey + ".selected", String.valueOf(setting.isSelected()));
+            
+            // Save all configurations
+            Map<String, String> configs = new HashMap<>(setting.getConfigurations()); // Create a copy
+            for (Map.Entry<String, String> entry : configs.entrySet()) {
+                properties.setProperty(baseKey + ".config." + entry.getKey(), entry.getValue());
+            }
+        }
+        saveProperties();
+    }
+
+    public Setting getActiveSetting() {
+        return loadSettings().stream()
+            .filter(Setting::isSelected)
+            .findFirst()
+            .orElse(null);
+    }
+}
