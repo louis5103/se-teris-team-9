1ad43576f81c59673a7192dd42068d3e30770625
JooseongMo0n <jsmoon4738@gmail.com>
2025-10-11 19:34:17 +0900

    Fix: Add  Annotation

--- tetris-backend/src/main/java/seoultech/se/backend/score/ScoreRepository.java
+++ tetris-backend/src/main/java/seoultech/se/backend/score/ScoreRepository.java
@@ -1,7 +1,9 @@
 package seoultech.se.backend.score;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ScoreRepository extends JpaRepository<ScoreEntity, Long> {
 
     
