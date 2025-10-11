10262862bc503a8aece7ec308271f5d25a74adaf
JooseongMo0n <jsmoon4738@gmail.com>
2025-10-11 20:31:34 +0900

    Refacto: UserController

---
+++ tetris-backend/src/main/java/seoultech/se/backend/user/LoginRequestDto.java
@@ -0,0 +1,8 @@
+package seoultech.se.backend.user;
+
+import lombok.Getter;
+
+@Getter
+public class LoginRequestDto {
+    
+}
--- tetris-backend/src/main/java/seoultech/se/backend/user/UserController.java
+++ tetris-backend/src/main/java/seoultech/se/backend/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
@@ -25,5 +27,36 @@ public class UserController {
         SignUpResultDto dto = userService.signUp(newUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(dto);
     }
+
+    /**
+     * 로그인
+     */
+    @GetMapping("/login")
+    public String login(@RequestBody LoginRequestDto user) {
+        return new String();
+    }
+
+    /**
+     * 로그아웃
+     */
+    @PostMapping("logout")
+    public String postMethodName(Long id) {
+        //TODO: process POST request
+        
+        return new String();
+    }
+    
+
+
+    /**
+     * 회원 삭제
+     */
+    @DeleteMapping("/delete")
+    public String deleteAccount(Long id) {
+        
+        return new String();
+    }
+    
+    
     
 }
