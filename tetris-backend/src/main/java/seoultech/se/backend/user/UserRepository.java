f8a7fc3c2a56bf2ace00d8fbe4e28452000d8e81
Jooseong Moon <99587187+JooseongMo0n@users.noreply.github.com>
2025-10-11 20:24:46 +0900

    Fix/Repository Annotation 추가 (#79)
    * Get ScoreBoard 기능 구현
    
    * Score 기능 구현
    
    * Score 기능 구현
    
    * 동기화
    
    * Score-Requirement1- Refactoring
    
    * Score-Requirement1-Refactoring
    
    * create Docker-compose.yml
    
    * Database 교체 완료
    
    * SignUp 기능 구현
    
    * SignUp 기능 구현
    
    * score 소문자
    
    * Fix: rename directory to lowercase
    
    * Fix: Add  Annotation

--- tetris-backend/src/main/java/seoultech/se/backend/user/UserRepository.java
+++ tetris-backend/src/main/java/seoultech/se/backend/user/UserRepository.java
@@ -3,7 +3,9 @@ package seoultech.se.backend.user;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     
     Optional<UserEntity> findByEmail(String email);
