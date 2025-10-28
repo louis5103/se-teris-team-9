7c08a55c922734aa58efb75136cb8a37374af8d8
JooseongMo0n <jsmoon4738@gmail.com>
2025-10-28 11:16:36 +0900

    Feat/109/Settings 기능 구현

---
+++ tetris-backend/src/main/java/seoultech/se/backend/settings/SettingRequestDto.java
@@ -0,0 +1,23 @@
+package seoultech.se.backend.settings;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SettingRequestDto {
+    
+    private String settingsName;
+    private String email;
+
+    private ScreenSize screenSize;
+    private ColorMode colorMode;
+    private int musicVolume;
+
+    private String leftKey;
+    private String rightKey;
+    private String downKey;
+    private String hardDownKey;
+    private String rotateKey;
+
+}
